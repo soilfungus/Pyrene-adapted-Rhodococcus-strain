0330ac64c2c689a10b0b9a9cb28bee50d3409eca
soilfungus <calcaneohg@gmail.com>
2026-05-06 18:28:45 -0600

    Assembly_Results.py

--- Assembly_Results.py
+++ Assembly_Results.py
@@ -1,7 +1,7 @@
 from Bio import SeqIO
 from Bio.SeqUtils import gc_fraction
 
-# ── Change this to your actual file name ──
+# Change this to your actual file name
 fasta_file = "ad.fasta"
 
 # Collect stats
